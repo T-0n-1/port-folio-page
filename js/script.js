b9142423be0328b4059f783c6a70790632838694
Toni Mertanen <t.mertanen@hotmail.com>
2023-02-27 23:37:16 +0200

    Nav page responsiveness ok

--- js/script.js
+++ js/script.js
@@ -1,68 +1,49 @@
 "use strict";
 
+let main = document.getElementById("main");
+let nav = document.getElementById("nav");
+let navbutton = document.getElementById("navbutton");
+let mainbutton1 = document.getElementById("mainbutton1");
+let mainbutton2 = document.getElementById("mainbutton2");
+let mainbutton3 = document.getElementById("mainbutton3");
+
+function toMain() {
+    nav.style.display = "none";
+    main.style.display = "grid";
+}
+
+function toNav() {
+    nav.style.display = "grid";
+    main.style.display = "none";
+}
+
 /* TO CHECK WHETHER THE CURSOR IS HIDDEN OR NOT */
-var element = document.documentElement;
-var style = getComputedStyle(element);
+let element = document.documentElement;
+let style = getComputedStyle(element);
 if (style.cursor === 'none') {
-  // The cursor is hidden
-  var main = document.getElementById("main");
-  var nav = document.getElementById("nav");
-  var navbutton = document.getElementById("navbutton");
-  var mainbutton1 = document.getElementById("mainbutton1");
-  var mainbutton2 = document.getElementById("mainbutton2");
-  var mainbutton3 = document.getElementById("mainbutton3");
-  navbutton.addEventListener("click", function () {
-    navbutton.style.transform = 'rotate(-10deg)';
-    setTimeout(function () {
-      main.style.display = "none";
-      nav.style.display = "grid";
-    }, 5000);
-  });
-  mainbutton1.addEventListener("click", function () {
-    mainbutton1.style.transform = 'rotate(-10deg)';
-    setTimeout(function () {
-      nav.style.display = "none";
-      main.style.display = "grid";
-    }, 300);
-  });
-  mainbutton2.addEventListener("click", function () {
-    mainbutton2.style.transform = 'rotate(-10deg)';
-    setTimeout(function () {
-      nav.style.display = "none";
-      main.style.display = "grid";
-    }, 300);
-  });
-  mainbutton3.addEventListener("click", function () {
-    mainbutton3.style.transform = 'rotate(-10deg)';
-    setTimeout(function () {
-      nav.style.display = "none";
-      main.style.display = "grid";
-    }, 300);
-  });
+    // The cursor is hidden
+    navbutton.addEventListener("touchstart", function () {
+        navbutton.style.transform = 'rotate(-10deg)';
+        setTimeout(toNav, 10000);
+    });
+    mainbutton1.addEventListener("touchstart", function () {
+        mainbutton1.style.transform = 'rotate(-10deg)';
+        setTimeout(toMain, 10000);
+    });
+    mainbutton2.addEventListener("touchstart", function () {
+        mainbutton2.style.transform = 'rotate(-10deg)';
+        setTimeout(toMain, 10000);
+    });
+    mainbutton3.addEventListener("touchstart", function () {
+        mainbutton3.style.transform = 'rotate(-10deg)';
+        setTimeout(toMain, 30000);
+    });
 } else {
-  // The cursor is visible
-  var _main = document.getElementById("main");
-  var _nav = document.getElementById("nav");
-  var _navbutton = document.getElementById("navbutton");
-  var _mainbutton = document.getElementById("mainbutton1");
-  var _mainbutton2 = document.getElementById("mainbutton2");
-  var _mainbutton3 = document.getElementById("mainbutton3");
-  _navbutton.addEventListener("click", function () {
-    _main.style.display = "none";
-    _nav.style.display = "grid";
-  });
-  _mainbutton.addEventListener("click", function () {
-    _nav.style.display = "none";
-    _main.style.display = "grid";
-  });
-  _mainbutton2.addEventListener("click", function () {
-    _nav.style.display = "none";
-    _main.style.display = "grid";
-  });
-  _mainbutton3.addEventListener("click", function () {
-    _nav.style.display = "none";
-    _main.style.display = "grid";
-  });
+    // The cursor is visible
+    navbutton.addEventListener("click", toNav);
+    mainbutton1.addEventListener("click", toMain);
+    mainbutton2.addEventListener("click", toMain);
+    mainbutton3.addEventListener("click", toMain);
 }
 
 //# sourceMappingURL=script.js.map
