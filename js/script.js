f716db7dbea5bba960d52583006a75039108ca2d
Toni Mertanen <t.mertanen@hotmail.com>
2023-03-06 00:11:29 +0200

    Elements return to original pos after animation

--- js/script.js
+++ js/script.js
@@ -19,19 +19,34 @@ function toNav() {
 
 navbutton.addEventListener("touchstart", function () {
     navbutton.style.transform = 'rotate(-10deg)';
-    setTimeout(toNav, 700);
+    setTimeout(function () {
+        toNav();
+        navbutton.style.transform = 'none'; // reset the transform property
+    }, 700);
 });
+
 mainbutton1.addEventListener("touchstart", function () {
     mainbutton1.style.transform = 'rotate(-10deg)';
-    setTimeout(toMain, 700);
+    setTimeout(function () {
+        toMain();
+        mainbutton1.style.transform = 'none'; // reset the transform property
+    }, 700);
 });
+
 mainbutton2.addEventListener("touchstart", function () {
     mainbutton2.style.transform = 'rotate(-10deg)';
-    setTimeout(toMain, 700);
+    setTimeout(function () {
+        toMain();
+        mainbutton2.style.transform = 'none'; // reset the transform property
+    }, 700);
 });
+
 mainbutton3.addEventListener("touchstart", function () {
     mainbutton3.style.transform = 'rotate(-10deg)';
-    setTimeout(toMain, 700);
+    setTimeout(function () {
+        toMain();
+        mainbutton3.style.transform = 'none'; // reset the transform property
+    }, 700);
 });
 
 //# sourceMappingURL=script.js.map
