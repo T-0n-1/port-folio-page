0b049f431465df705541a379c0751a3b39e26340
Toni Mertanen <t.mertanen@hotmail.com>
2023-03-06 03:22:32 +0200

    Rolling navbutton

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@
 let main = document.getElementById("main");
 let nav = document.getElementById("nav");
 let navbutton = document.getElementById("navbutton");
+let menubutton = document.getElementById("menubutton");
 let mainbutton1 = document.getElementById("mainbutton1");
 let mainbutton2 = document.getElementById("mainbutton2");
 let mainbutton3 = document.getElementById("mainbutton3");
+let menuopen = false;
 
 function toMain() {
     nav.style.display = "none";
@@ -17,12 +19,31 @@ function toNav() {
     main.style.display = "none";
 }
 
-navbutton.addEventListener("click", function () {
-    navbutton.style.transform = 'rotate(-10deg)';
+/* navbutton.addEventListener("click", function () {
+    navbutton.style.transform = 'rotate(1080deg)';
     setTimeout(function () {
         toNav();
         navbutton.style.transform = 'none'; // reset the transform property
     }, 700);
+}); original code */
+
+navbutton.addEventListener("click", function () {
+    if (menuopen == false) {
+        navbutton.style.transform = 'rotate(360deg)'; // Reset rotation angle and apply new rotation angle
+        setTimeout(function () {
+            menubutton.classList.remove("fa-bars");
+            menubutton.classList.add("fa-times");
+            menuopen = true;
+        }, 500);
+    } else {
+        navbutton.style.transform = 'rotate(-360deg)'; // Reset rotation angle and apply new rotation angle
+        setTimeout(function () {
+            menubutton.classList.remove("fa-times");
+            menubutton.classList.add("fa-bars");
+            menuopen = false;
+            /*toNav();*/
+        }, 500);
+    }
 });
 
 mainbutton1.addEventListener("click", function () {
